4a57b32fa348993e3f7cbc699b959f77a9ef9ca7
Rich Brown <richb.hanover@gmail.com>
2025-08-28 22:09:14 -0400

    Remove empty whitespace in NewToast

--- src/components/NewToast.tsx
+++ src/components/NewToast.tsx
@@ -83,7 +83,6 @@ export default function NewToast({ onClose, toastIsReady }: NewToastProps) {
           <Toast.Title className="font-bold">{toastHeader}</Toast.Title>
           {/* Convert \n into actual <br /> elements */}
           <Toast.Description className="text-sm text-gray-800 leading-relaxed">
-            {" "}
             {toastStatus.split("\n").map((line, index) => (
               <span key={index}>
                 <div>{line}</div>
